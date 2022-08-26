62629fbe44bf694671ecbe83cc352d2ed9584d07
nwebpro <nwebpro.work@gmail.com>
2022-08-27 02:03:41 +0600

    display 8 posts

--- dist/js/post.js
+++ dist/js/post.js
@@ -6,21 +6,6 @@ function loadPost(){
 
 function displayPost(data){
     const postContainer = document.getElementById('post-container');
-    // for (const post of data) {
-    //     const div = document.createElement('div');
-    //     div.innerHTML = `
-    //     <a href="#">
-    //         <img class="rounded-t-lg" src="./dist/images/image.jpg" alt="">
-    //     </a>
-    //     <div class="p-5">
-    //         <a href="#">
-    //             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize">${post.title}</h5>
-    //         </a>
-    //         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 capitalize">${post.body}</p>
-    //     </div>
-    //     `;
-    //     postContainer.appendChild(div);
-    // }
     for (let i = 0; i <= 7; i++) {
         const div = document.createElement('div');
         div.innerHTML = `
