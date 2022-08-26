c0918c250c7665d718bd1eb9946d8e10d30dc92e
nwebpro <nwebpro.work@gmail.com>
2022-08-27 02:00:16 +0600

    display 8 posts

--- dist/js/post.js
+++ dist/js/post.js
@@ -6,7 +6,22 @@ function loadPost(){
 
 function displayPost(data){
     const postContainer = document.getElementById('post-container');
-    for (const post of data) {
+    // for (const post of data) {
+    //     const div = document.createElement('div');
+    //     div.innerHTML = `
+    //     <a href="#">
+    //         <img class="rounded-t-lg" src="./dist/images/image.jpg" alt="">
+    //     </a>
+    //     <div class="p-5">
+    //         <a href="#">
+    //             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize">${post.title}</h5>
+    //         </a>
+    //         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 capitalize">${post.body}</p>
+    //     </div>
+    //     `;
+    //     postContainer.appendChild(div);
+    // }
+    for (let i = 0; i <= 7; i++) {
         const div = document.createElement('div');
         div.innerHTML = `
         <a href="#">
@@ -14,9 +29,9 @@ function displayPost(data){
         </a>
         <div class="p-5">
             <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize">${post.title}</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize">${data[i].title}</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 capitalize">${post.body}</p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 capitalize">${data[i].body}</p>
         </div>
         `;
         postContainer.appendChild(div);
