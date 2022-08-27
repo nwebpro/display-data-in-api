78506f73f180ebda25dbfd9a261693063cfe2260
nwebpro <nwebpro.work@gmail.com>
2022-08-27 11:46:39 +0600

    Post layout Update

--- dist/js/post.js
+++ dist/js/post.js
@@ -8,15 +8,16 @@ function displayPost(data){
     const postContainer = document.getElementById('post-container');
     for (let i = 0; i <= 7; i++) {
         const div = document.createElement('div');
+        div.classList.add('mb-5', 'border', 'border-gray-200', 'shadow-m', 'bg-white', 'rounded-lg');
         div.innerHTML = `
         <a href="#">
             <img class="rounded-t-lg" src="./dist/images/image.jpg" alt="">
         </a>
         <div class="p-5">
             <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize">${data[i].title}</h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 capitalize">${data[i].title}</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 capitalize">${data[i].body}</p>
+            <p class="mb-3 font-normal text-gray-700 capitalize">${data[i].body}</p>
         </div>
         `;
         postContainer.appendChild(div);
